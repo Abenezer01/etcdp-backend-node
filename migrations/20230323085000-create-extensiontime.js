b1f9adf03d33e7a61f884dfc09122c36838ee65e
habtesoft <habtesoftaau@gmail.com>
2023-03-23 08:54:31 +0000

    extension time model and migration

---
+++ migrations/20230323085000-create-extensiontime.js
@@ -0,0 +1,48 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('extensiontimes', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      parent_id: {
+        type: Sequelize.UUID
+      },
+      project_id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: 'projects',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      number_of_days: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      reason: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('extensiontimes');
+  }
+};
---
+++ models/extensiontime.js
@@ -0,0 +1,45 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class extensiontime extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  extensiontime.init({
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4
+    },
+    title: {
+      type: DataTypes.STRING
+    },
+    parent_id: {
+      type: DataTypes.UUID
+    },
+    project_id: {
+      type: DataTypes.UUID,
+      allowNull:false
+    },
+    number_of_days: {
+      type: DataTypes.INTEGER,
+      allowNull:false
+    },
+    reason: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+  }, {
+    sequelize,
+    modelName: 'extensiontime',
+  });
+  return extensiontime;
+};
