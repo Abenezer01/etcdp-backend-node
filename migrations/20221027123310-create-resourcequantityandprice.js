1f50765fc0797e0c5f5bde42f70f7594d0c962e2
Abenezer01 <abenezerseyoum7@gmail.com>
2023-11-10 17:36:15 +0000

    migration order fix

--- migrations/20221027123310-create-resourcequantityandprice.js
+++ migrations/20221027123310-create-resourcequantityandprice.js
@@ -57,7 +57,7 @@ module.exports = {
         type: Sequelize.STRING,
       },
       date: {
-        type: Sequelize.DATETIME,
+        type: Sequelize.DATE,
       },
       datasource: {
         type: Sequelize.STRING,
