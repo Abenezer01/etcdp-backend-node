106fb18fa732ca66c46a827e07494dfb4e1e5aaf
habtesoft <habtesoftaau@gmail.com>
2024-05-27 10:31:03 +0000

    variable change

--- utils/userDataFromToken.js
+++ utils/userDataFromToken.js
@@ -5,13 +5,13 @@ let ACCESS_TOKEN_KEY = process.env.ACCESS_TOKEN_KEY;
 const userData = async (req, res, next) => {
   // try {
 
-  const userTokenn = req.headers.authorization;
+  const userToken = req.headers.authorization;
   // if (!userTokenn) {
   //   return res.status(401).json({ error: "Authorization token is missing" });
   // }
   // const [, userTokenn] = tokenString.split(' ');
   let decodetoken;
-  decodetoken = jwt.verify(userTokenn, ACCESS_TOKEN_KEY);
+  decodetoken = jwt.verify(userToken, ACCESS_TOKEN_KEY);
   usrID = decodetoken.id;
   departmentID = decodetoken.department_id;
   position_id = decodetoken.position_id;
