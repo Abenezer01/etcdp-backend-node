ceb0a84b253051bead3ffe15bee5c705b75aa178
habtesoft <habtesoftaau@gmail.com>
2024-06-11 10:42:27 +0300

    ECDMS-12 document module crud optimization

--- controllers/document/documentCategoryController.js
+++ controllers/document/documentCategoryController.js
@@ -1,9 +1,10 @@
-const { DocumentCategory, Sequelize } = require("../../models");
+const { DocumentCategory, DocumentSubCategory, Sequelize } = require("../../models");
 const usrData = require("../../utils/userDataFromToken");
 const actionHelper = require("../utils/action-helper");
 const paginationHelper = require("../utils/pagination-helper")
 const Op = Sequelize.Op;
-const paginate = require("../../utils/pagination");
+const { getRecordById, saveRecord, updateRecord, deleteRecord } = require('../utils/format-helper');
+
 const dotenv = require("dotenv");
 dotenv.config();
 let self = {};
@@ -26,22 +27,21 @@ self.getAll = async (req, res) => {
 };
 
 self.get = async (req, res) => {
-  try {
-    let id = req.params.id;
-    let data = await DocumentCategory.findOne({
-      where: {
-        id: id,
-      },
-    });
-    return res.status(200).json({
-      data: data ? data : {},
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
+  getRecordById(DocumentCategory, req, res);
 };
+
+self.save = async (req, res) => {
+  saveRecord(DocumentCategory, req, res);
+};
+
+self.update = async (req, res) => {
+  updateRecord(DocumentCategory, req, res);
+};
+
+self.delete = async (req, res) => {
+  deleteRecord(DocumentCategory, req, res);
+};
+
 
 self.search = async (req, res) => {
   try {
@@ -60,95 +60,27 @@ self.search = async (req, res) => {
     });
   }
 };
+
 self.getCRCBydocumentTypeId = async (req, res) => {
-  let { page, size, order } = req.query;
-  let id = req.params.id;
-  //console.log("The page", page, size)
-  if (page == null && size == null) {
-    (page = process.env.page), (size = process.env.size);
-  }
-  if (order == null) {
-    order = process.env.order;
-  }
-  const { limit, offset } = paginate.getPagination(page, size);
-  DocumentCategory
-    .findAndCountAll({
-      limit,
-      offset,
-      order: [["createdAt", order]],
-      where: {
-        documenttype_id: id,
-      },
-      include: ["documentsubcategories"],
-    })
-    .then((data) => {
-      const response = paginate.getPagingData(data, page, limit);
-      res.send(response);
-    })
-    .catch((err) => {
-      res.status(500).send({
-        message: err.message || "Some error occurred while retrieving data.",
-      });
-    });
-};
-self.save = async (req, res) => {
+  const { id } = req.params;
   try {
-    let usr = await usrData.userData(req, res);
-    let body = req.body;
-    if (usr) {
-      let data = await DocumentCategory.create(body);
-      if (data) {
-        let us = usr.usrID;
-        await actionHelper.saveActionState(
-          data.id,
-          "DocumentCategory",
-          "REGISTER",
-          us,
-          req,
-          res
-        );
-      }
-      return res.json(data);
-    }
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-};
+    const whereCondition = { documenttype_id: id }
 
-self.update = async (req, res) => {
-  try {
-    let id = req.params.id;
-    let body = req.body;
-    let data = await DocumentCategory.update(body, {
-      where: {
-        id: id,
-      },
-    });
-    return res.status(200).json({
-      message: "Success",
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-};
+    const includeOptions = [
+      { model: DocumentSubCategory, as: 'documentsubcategories' } // Example association
+    ];
 
-self.delete = async (req, res) => {
-  try {
-    let id = req.params.id;
-    let data = await DocumentCategory.destroy({
-      where: {
-        id: id,
-      },
-    });
-    return res.json(data);
+    const paginatedResult = await paginationHelper(DocumentCategory, req, whereCondition, includeOptions);
+
+    // Use the response formatter to send the success response
+    res.apiSuccess({
+      data: paginatedResult.data,
+      total: paginatedResult.total,
+    }, paginatedResult.pagination);
+
   } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
+    console.error("Error in getAll method:", error);
+    res.apiError(error);
   }
 };
 
