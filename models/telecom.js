ad341b728b7c3e422e12dff6080e6237979329ee
Habtamu Emyou <41988126+Habtesoft@users.noreply.github.com>
2025-01-04 12:53:57 +0300

    Add files via upload

---
+++ models/telecom.js
@@ -0,0 +1,46 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Telecom extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(/* models*/) {
+      // define association here
+    }
+  }
+  Telecom.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      parent_id: DataTypes.UUID,
+      project_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      specifications: DataTypes.TEXT,
+      coverage_area: DataTypes.DOUBLE,
+      no_of_families_coverage: DataTypes.DOUBLE,
+      service_period: DataTypes.DATE,
+      inauguration_date: DataTypes.DATE,
+      revision_no: DataTypes.INTEGER,
+    },
+    {
+      createdAt: "created_at",
+      updatedAt: "updated_at" ,     
+      sequelize,
+      modelName: "Telecom",
+      tableName: "telecoms"
+    }
+  );
+  return Telecom;
+};
---
+++ models/totalemployee.js
@@ -0,0 +1,72 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class TotalEmployee extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(/* models*/) {
+      // define association here
+    }
+  }
+  TotalEmployee.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      parent_id: DataTypes.UUID,
+      stakeholder_id: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      year: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      domain: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      department_name: DataTypes.UUID,
+      nationality: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      male: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      female: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      total_employees: {
+        type: DataTypes.VIRTUAL,
+        get() {
+          return this.male + this.female;
+        },
+      },
+
+      file_id: DataTypes.UUID,
+      revision_no: {
+        type: DataTypes.INTEGER,
+      },
+    },
+    {
+      createdAt: "created_at",
+      updatedAt: "updated_at" ,     
+      sequelize,
+      modelName: "TotalEmployee",
+      tableName: "totalemployees"
+    }
+  );
+  return TotalEmployee;
+};
